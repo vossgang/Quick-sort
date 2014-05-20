fada8ddd5931816aa5a07e620450833fab17c8df
vossgang <vossgang@gmail.com>
2014-05-20 12:44:05 -0700

    Initial Commit

---
+++ QuickSort/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  QuickSort
+//
+//  Created by Matthew Voss on 5/20/14.
+//  Copyright (c) 2014 matt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "mvAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([mvAppDelegate class]));
+    }
+}
---
+++ QuickSort/mvViewController.h
@@ -0,0 +1,13 @@
+//
+//  mvViewController.h
+//  QuickSort
+//
+//  Created by Matthew Voss on 5/20/14.
+//  Copyright (c) 2014 matt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface mvViewController : UIViewController
+
+@end
